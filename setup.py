bb97438394fe657f3a7f47d9fa0475252c4536fa
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2008-03-19 16:04:57 +0000

    `python setup.py install` now runs `nrnivmodl` to compile the NEURON .mod files and puts the result in `pyNN.hoc`.
    git-svn-id: https://neuralensemble.org/svn/PyNN/trunk@250 1a7b9c28-352d-0410-8541-c879d6df8dcf

--- setup.py
+++ setup.py
@@ -1,7 +1,43 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.command.build import build as _build
+import os
 
+class build(_build):
+    """Add nrnivmodl to the end of the build process."""
+
+    def run(self):
+        _build.run(self)
+        nrnivmodl = self.find_nrnivmodl()
+        if nrnivmodl:
+            print "nrnivmodl found at", nrnivmodl
+            import subprocess
+            p = subprocess.Popen(nrnivmodl, shell=True, stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                         close_fds=True, cwd=os.path.join(os.getcwd(),'build/lib/pyNN/hoc'))
+            result = p.wait()
+            # test if nrnivmodl was successful
+            if result != 0:
+                errorMsg = p.stderr.readlines()
+                print "Unable to compile NEURON extensions. Error message was:"
+                print errorMsg
+            else:
+                print "Successfully compiled NEURON extensions."
+        else:
+            print "Unable to find nrnivmodl. It will not be possible to use the pyNN.neuron module."
+        
+    def find_nrnivmodl(self):
+        """Try to find the nrnivmodl executable."""
+        path = os.environ.get("PATH", "").split(os.pathsep)
+        nrnivmodl = ''
+        for dir_name in path:
+            abs_name = os.path.abspath(os.path.normpath(os.path.join(dir_name, "nrnivmodl")))
+            if os.path.isfile(abs_name):
+                nrnivmodl = abs_name
+                break
+        return nrnivmodl
+      
 setup(
     name = "PyNN",
     version = "0.4.0alpha",
@@ -14,4 +50,6 @@ setup(
     license = "CeCILL",
     keywords = "computational neuroscience simulation neuron nest pcsim neuroml",
     url = "http://neuralensemble.org/PyNN/",
-     )
+    cmdclass = {'build': build},
+)
+
