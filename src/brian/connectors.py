1af8708676439ee0377c7f494840e493190be4d9
Pierre Yger <pierre.yger@unic.cnrs-gif.fr>
2008-05-07 10:30:31 +0000

    Start the pyNN.brian implementation. The VA benchmarks is almost working in CUBA, but low level API is not consistent at all....
    git-svn-id: https://neuralensemble.org/svn/PyNN/trunk@314 1a7b9c28-352d-0410-8541-c879d6df8dcf

---
+++ src/brian/connectors.py
@@ -0,0 +1,64 @@
+# ==============================================================================
+# Connection method classes for nest1
+# $Id: connectors.py 294 2008-04-04 12:07:56Z apdavison $
+# ==============================================================================
+
+from pyNN import common
+from pyNN.brian.__init__ import numpy
+import brian_no_units_no_warnings
+import brian
+from pyNN.random import RandomDistribution, NativeRNG
+from math import *
+
+class AllToAllConnector(common.AllToAllConnector):    
+    
+    def connect(self, projection):
+        if projection.synapse_type == "excitatory":
+            projection._connections = brian.Connection(projection.pre.cell, projection.post.cell,'ge', delay=self.delays)
+        else:
+            projection._connections = brian.Connection(projection.pre.cell, projection.post.cell,'gi', delay=self.delays)
+        projection._connections.connect_full(projection.pre.cell,projection.post.cell, weight=self.weights)
+        return projection._connections.W.getnnz()
+
+class OneToOneConnector(common.OneToOneConnector):
+    
+    def connect(self, projection):
+        raise Exception("Not implemented yet !")
+    
+class FixedProbabilityConnector(common.FixedProbabilityConnector):
+    
+    def connect(self, projection):
+        if projection.synapse_type == "excitatory":
+            projection._connections = brian.Connection(projection.pre.cell,projection.post.cell,'ge', delay=self.delays)
+        else:
+            projection._connections = brian.Connection(projection.pre.cell,projection.post.cell,'gi', delay=self.delays)
+        projection._connections.connect_random(projection.pre.cell,projection.post.cell, self.p_connect, weight=self.weights)
+        return projection._connections.W.getnnz()
+    
+class DistanceDependentProbabilityConnector(common.DistanceDependentProbabilityConnector):
+    
+    def connect(self, projection):
+        raise Exception("Not implemented yet !")
+    
+
+class FixedNumberPreConnector(common.FixedNumberPreConnector):
+    
+    def connect(self, projection):
+        raise Exception("Not implemented yet !")
+
+class FixedNumberPostConnector(common.FixedNumberPostConnector):
+    
+    def connect(self, projection):
+        raise Exception("Not implemented yet !")
+    
+
+class FromListConnector(common.FromListConnector):
+    
+    def connect(self, projection):
+        raise Exception("Not implemented yet !")
+            
+
+class FromFileConnector(common.FromFileConnector):
+    
+    def connect(self, projection):
+        raise Exception("Not implemented yet !")
