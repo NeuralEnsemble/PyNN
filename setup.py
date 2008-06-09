9a7dc937af0bfbf8ae03eed2500db5268423e4f7
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2008-06-09 14:36:04 +0000

    Added more metadata to setup.py
    git-svn-id: https://neuralensemble.org/svn/PyNN/branches/0.4@349 1a7b9c28-352d-0410-8541-c879d6df8dcf

--- setup.py
+++ setup.py
@@ -45,11 +45,29 @@ setup(
     packages = ['pyNN','pyNN.nest2','pyNN.nest1','pyNN.pcsim','pyNN.neuron'],
     package_data = {'pyNN': ['hoc/*.hoc', 'hoc/*.mod']},
     author = "The NeuralEnsemble Community",
-    author_email = "pynn@neuralensemble.org",
+    author_email = "davison at unic cnrs-gif fr",
     description = "A Python package for simulator-independent specification of neuronal network models",
-    license = "CeCILL",
+    long_description = """In other words, you can write the code for a model once, using the PyNN API, and then run it without modification on any simulator that PyNN supports (currently NEURON, NEST and PCSIM).
+
+The API has two parts, a low-level, procedural API, similar to that in PyNEST (functions ``create()``, ``connect()``, ``set()``, ``record()``, ``record_v()``), and a high-level, object-oriented API (classes ``Population`` and ``Projection``, which have methods like ``set()``, ``record()``, ``setWeights()``, etc.).
+
+The low-level API is good for small networks, and perhaps gives more flexibility. The high-level API is good for hiding the details and the book-keeping, and is intended eventually to have a one-to-one mapping with NeuroML.
+
+The other thing that is required to write a model once and run it on multiple simulators is standard cell models. PyNN translates standard cell-model names and parameter names into simulator-specific names, e.g. standard model ``IF_curr_alpha`` is ``iaf_neuron`` in NEST and ``StandardIF`` in NEURON, while ``SpikeSourcePoisson`` is a ``poisson_generator`` in NEST and a ``NetStim`` in NEURON.
+
+PyNN is a work in progress, but is already being used for several large-scale simulation projects. The current stable release of the API is 0.4, but if you wish to stay up to date with the latest features, you should use the version from the Subversion repository.
+""",
+    license = "CeCILL http://www.cecill.info/",
     keywords = "computational neuroscience simulation neuron nest pcsim neuroml",
     url = "http://neuralensemble.org/PyNN/",
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Science/Research',
+                   'License :: Other/Proprietary License',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering'],
     cmdclass = {'build': build},
 )
 
