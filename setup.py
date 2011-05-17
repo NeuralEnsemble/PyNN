fc80cb928d382f1373d3f338aa9f7efde199b274
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2011-05-17 09:00:06 +0000

    Backed-out unintended change to src/neuron/nineml.py made in r957
    git-svn-id: https://neuralensemble.org/svn/PyNN/trunk@963 1a7b9c28-352d-0410-8541-c879d6df8dcf

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     author = "The PyNN team",
     author_email = "pynn@neuralensemble.org",
     description = "A Python package for simulator-independent specification of neuronal network models",
-        long_description = """In other words, you can write the code for a model once, using the PyNN API and the Python_ programming language, and then run it without modification on any simulator that PyNN supports (currently NEURON, NEST, PCSIM and Brian).
+        long_description = """In other words, you can write the code for a model once, using the PyNN API and the Python programming language, and then run it without modification on any simulator that PyNN supports (currently NEURON, NEST, PCSIM and Brian).
 
 The API has two parts, a low-level, procedural API (functions ``create()``, ``connect()``, ``set()``, ``record()``, ``record_v()``), and a high-level, object-oriented API (classes ``Population`` and ``Projection``, which have methods like ``set()``, ``record()``, ``setWeights()``, etc.). 
 
