863b37fded03cb2215fc84c282cfeb9b965912b6
Eilif Muller <eilif.mueller@epfl.ch>
2011-07-26 14:21:50 +0000

    Fixes to NEST NineML code to update to latest python lib9ml API
    git-svn-id: https://neuralensemble.org/svn/PyNN/trunk@987 1a7b9c28-352d-0410-8541-c879d6df8dcf

--- src/nest/nineml.py
+++ src/nest/nineml.py
@@ -19,7 +19,7 @@ Constants:
 
 from __future__ import absolute_import # Not compatible with Python 2.4
 import subprocess
-import neuron
+#import neuron
 from pyNN.models import BaseCellType
 #from pyNN.nineml.cells import _build_nineml_celltype
 from pyNN.nineml.cells import CoBaSyn 
@@ -65,23 +65,35 @@ class _nest_build_nineml_celltype(type):
     """
     def __new__(cls, name, bases, dct):
         
-        import nineml.abstraction_layer as nineml
-        from nineml.abstraction_layer import models               
+        import nineml.abstraction_layer as al
+        from nineml.abstraction_layer import flattening, writers, component_modifiers
         import nest
-        
+
         #Extract Parameters Back out from Dict:
         nineml_model = dct['nineml_model']
         synapse_components = dct['synapse_components']
 
-        # Reduce the model:                    
-        reduced_component = models.reduce_to_single_component( nineml_model,
-                                                               componentname=name )
-        reduced_component.backsub_all()
-        #reduced_component.backsub_aliases()
-        #reduced_component.backsub_equations()
+        # Flatten the model:
+        assert isinstance(nineml_model, al.ComponentClass)
+        if nineml_model.is_flat():
+            flat_component = nineml_model
+        else:
+            flat_component = flattening.flatten( nineml_model,name )
+        
+        # Make the substitutions:
+        flat_component.backsub_all()
+        #flat_component.backsub_aliases()
+        #flat_component.backsub_equations()
 
-        reduced_component.short_description = "Auto-generated 9ML neuron model for PyNN.nest"
-        reduced_component.long_description = "Auto-generated 9ML neuron model for PyNN.nest"
+        # Close any open reduce ports:
+        component_modifiers.ComponentModifier.close_all_reduce_ports(component = flat_component)
+
+
+        flat_component.short_description = "Auto-generated 9ML neuron model for PyNN.nest"
+        flat_component.long_description = "Auto-generated 9ML neuron model for PyNN.nest"
+
+        # Close any open reduce ports:
+        component_modifiers.ComponentModifier.close_all_reduce_ports(component = flat_component)
 
         # synapse ports:
         synapse_ports = []
@@ -90,19 +102,19 @@ class _nest_build_nineml_celltype(type):
             # TODO: model namespace look
             #syn_component = nineml_model[syn.namespace]
             syn_component = nineml_model.subnodes[syn.namespace]
-            recv_event_ports = list(syn_component.filter_ports(cls=nineml.EventPort, mode='recv'))
+            recv_event_ports = list(syn_component.query.event_recv_ports)
             # check there's only one
             if len(recv_event_ports)!=1:
                 raise ValueError, "A synapse component has multiple recv ports.  Cannot dis-ambiguate"
-            synapse_ports.append(syn.namespace+'_'+recv_event_ports[0].symbol)
+            synapse_ports.append(syn.namespace+'_'+recv_event_ports[0].name)
 
         
         # New:
-        dct["combined_model"] = reduced_component
+        dct["combined_model"] = flat_component
         # TODO: Override this with user layer
         #default_values = ModelToSingleComponentReducer.flatten_namespace_dict( parameters )
-        dct["default_parameters"] = dict( (name, 1.0) for name in reduced_component.parameters )
-        dct["default_initial_values"] = dict((name, 0.0) for name in reduced_component.state_variables)
+        dct["default_parameters"] = dict( (p.name, 1.0) for p in flat_component.parameters )
+        dct["default_initial_values"] = dict((s.name, 0.0) for s in flat_component.state_variables)
         dct["synapse_types"] = [syn.namespace for syn in synapse_components] 
         dct["standard_receptor_type"] = (dct["synapse_types"] == ('excitatory', 'inhibitory'))
         dct["injectable"] = True # need to determine this. How??
@@ -112,9 +124,9 @@ class _nest_build_nineml_celltype(type):
 
         
         # Recording from bindings:
-        dct["recordable"] = [port.name for port in reduced_component.analog_ports] + ['spikes', 'regime']
+        dct["recordable"] = [port.name for port in flat_component.analog_ports] + ['spikes', 'regime']
         # TODO bindings -> alias and support recording of them in nest template
-        #+ [binding.name for binding in reduced_component.bindings]
+        #+ [binding.name for binding in flat_component.bindings]
         
         dct["weight_variables"] = dict([ (syn.namespace,syn.namespace+'_'+syn.weight_connector )
                                          for syn in synapse_components ])
@@ -122,11 +134,11 @@ class _nest_build_nineml_celltype(type):
         logger.debug("Creating class '%s' with bases %s and dictionary %s" % (name, bases, dct))
 
         # TODO: UL configuration of initial regime.
-        initial_regime = reduced_component.regime_map.keys()[0]
+        initial_regime = flat_component.regimes_map.keys()[0]
 
         from nestbuilder import NestFileBuilder
         nfb = NestFileBuilder(  nest_classname = name, 
-                                component = reduced_component, 
+                                component = flat_component, 
                                 synapse_ports = synapse_ports,
                                 initial_regime =  initial_regime,
                                 initial_values = dct["default_initial_values"],
