ec31478aa9c55196e5c5a4b7b4700efb82b8b4ea
Sven Schrader <sven.schrader@kip.uni-heidelberg.de>
2012-02-06 10:03:28 +0000

    test commit
    git-svn-id: https://neuralensemble.org/svn/PyNN/trunk@1054 1a7b9c28-352d-0410-8541-c879d6df8dcf

--- src/nest/recording.py
+++ src/nest/recording.py
@@ -324,5 +324,5 @@ class Recorder(recording.Recorder):
             for id, l, r in zip(idx, left, right):
                 N[id] = r-l
         return N
-    
+   
 simulator.Recorder = Recorder # very inelegant. Need to rethink the module structure
