0c12577132d064535c13cd7675dc158edd01debc
Mikael Djurfeldt <mikael@djurfeldt.com>
2012-03-17 09:44:46 +0000

    Removed get_simulators and let setup return them instead. get_simulators used simulator names to identify simulator instances. However, there might be multiple instances of the same simulator.
    git-svn-id: https://neuralensemble.org/svn/PyNN/branches/music@1074 1a7b9c28-352d-0410-8541-c879d6df8dcf

--- examples/music_example.py
+++ examples/music_example.py
@@ -9,9 +9,10 @@ from pyNN import music
 
 vizapp = music.Config("vizapp", 1, "/path/to/vizapp", "args")
 
-music.setup(music.Config("neuron", 10), music.Config("nest", 20), vizapp)
+sim1, sim2 = music.setup(music.Config("neuron", 10),
+                         music.Config("nest", 20),
+                         vizapp)
 
-sim1, sim2 = music.get_simulators("neuron", "nest")
 sim1.setup(timestep=0.025)
 sim2.setup(timestep=0.1)
 cell_parameters = {"tau_m": 12.0, "cm": 0.8, "v_thresh": -50.0, "v_reset": -65.0}
@@ -25,6 +26,6 @@ e2e = sim1.Projection(pE, pE, connector(sim1), target="excitatory")
 e2i = music.Projection(pE, pI, connector(music), target="excitatory")
 i2i = sim2.Projection(pI, pI, connector(sim2), target="inhibitory")
 
-output = music.Port(pE, "spikes", vizapp, "pE_spikes_viz")
+output = music.Port(pE, "spikes", vizapp, "in")
 
 music.run(1000.0)
