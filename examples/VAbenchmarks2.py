aba7f695c609c313565264b45119940e1667e851
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2014-04-30 15:50:44 +0200

    Forgot to add these files to last commit.

--- examples/VAbenchmarks2.py
+++ examples/VAbenchmarks2.py
@@ -132,7 +132,7 @@ if benchmark == "COBA":
 
 print "%s Initialising membrane potential to random values..." % node_id
 rng = NumpyRNG(seed=rngseed, parallel_safe=parallel_safe)
-uniformDistr = RandomDistribution('uniform', [v_reset,v_thresh], rng=rng)
+uniformDistr = RandomDistribution('uniform', {'low': v_reset, 'high': v_thresh}, rng=rng)
 all_cells.initialize(v=uniformDistr)
 
 print "%s Connecting populations..." % node_id
