44d2d1fd5be96e71e186bd49ebe25f96e71e07f9
JoelChavas <joel.chavas@unic.cnrs-gif.fr>
2014-06-24 12:14:04 +0000

    working link to hardware PyNN backend. Clean implementation.

---
+++ src/hardware/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+hardware implementation of the PyNN API.
+It includes the submodules that stand on another directory.
+This solution is a clean way to make the submodules (brainscales, etc...) 
+be indeed submodules of hardware, even if they don't stand on the same directory
+
+:copyright: Copyright 2006-2013 by the PyNN team, see AUTHORS.
+:license: CeCILL, see LICENSE for details.
+
+"""
+
+from aux import get_path_to_analog_hardware_backend, import_all_submodules
+
+__path__.append(get_path_to_analog_hardware_backend())
+import_all_submodules(__path__)
