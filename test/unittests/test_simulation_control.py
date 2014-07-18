60e1ac3103a39db20d90742a5b9bd45ad89bac57
JoelChavas <joel.chavas@unic.cnrs-gif.fr>
2014-07-18 14:22:25 +0000

    test_simulation_control added

--- test/unittests/test_simulation_control.py
+++ test/unittests/test_simulation_control.py
@@ -24,9 +24,12 @@ if MPI:
 
 @register_class()
 class TestSimulationControl(unittest.TestCase):
-    
+
+    def runTest():
+        assert True
+        
     @register()
-    def test_setup(self):
+    def test_setup(self, sim=sim):
         self.assertRaises(Exception, sim.setup, min_delay=1.0, max_delay=0.9)
         self.assertRaises(Exception, sim.setup, mindelay=1.0)  # } common
         self.assertRaises(Exception, sim.setup, maxdelay=10.0) # } misspellings
@@ -34,25 +37,25 @@ class TestSimulationControl(unittest.TestCase):
         self.assertRaises(Exception, sim.setup, timestep=0.1, min_delay=0.09)
 
     @register()
-    def test_end(self):
+    def test_end(self, sim=sim):
         sim.setup()
         sim.end() # need a better test
     
     @register()
-    def test_run(self):
+    def test_run(self, sim=sim):
         sim.setup()
         self.assertEqual(sim.run(1000.0), 1000.0)
         self.assertEqual(sim.run(1000.0), 2000.0)
     
     @register()
-    def test_reset(self):
+    def test_reset(self, sim=sim):
         sim.setup()
         sim.run(1000.0)
         sim.reset()
         self.assertEqual(sim.get_current_time(), 0.0)
     
     @register()
-    def test_current_time(self):
+    def test_current_time(self, sim=sim):
         sim.setup(timestep=0.1)
         sim.run(10.1)
         self.assertEqual(sim.get_current_time(), 10.1)
@@ -60,22 +63,22 @@ class TestSimulationControl(unittest.TestCase):
         self.assertEqual(sim.get_current_time(), 33.5)
     
     @register()
-    def test_time_step(self):
+    def test_time_step(self, sim=sim):
         sim.setup(0.123, min_delay=0.246)
         self.assertEqual(sim.get_time_step(), 0.123)
     
     @register()
-    def test_min_delay(self):
+    def test_min_delay(self, sim=sim):
         sim.setup(0.123, min_delay=0.246)
         self.assertEqual(sim.get_min_delay(), 0.246)
     
     @register()
-    def test_max_delay(self):
+    def test_max_delay(self, sim=sim):
         sim.setup(max_delay=9.87)
         self.assertEqual(sim.get_max_delay(), 9.87)
 
     @register()
-    def test_callbacks(self):
+    def test_callbacks(self, sim=sim):
         total_time = 100.
         callback_steps = [10., 10., 20., 25.]
 
@@ -98,11 +101,11 @@ class TestSimulationControl(unittest.TestCase):
             for i in range(num_callbacks)))
     
     @unittest.skipUnless(MPI, "test requires mpi4py")
-    def test_num_processes(self):
+    def test_num_processes(self, sim=sim):
         self.assertEqual(sim.num_processes(), mpi_comm.size)
     
     @unittest.skipUnless(MPI, "test requires mpi4py")
-    def test_rank(self):
+    def test_rank(self, sim=sim):
         self.assertEqual(sim.rank(), mpi_comm.rank)
 
 
