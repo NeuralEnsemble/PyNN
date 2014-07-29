7356b5f27fc27d68e79fb61702ebb5af193020e9
JoelChavas <joel.chavas@unic.cnrs-gif.fr>
2014-07-29 14:13:51 +0000

    add sim.rank comparison in test_connectors_parallel

--- test/unittests/backends/glob_import.py
+++ test/unittests/backends/glob_import.py
@@ -1,12 +1,12 @@
 # encoding: utf-8
 
 from ..alias_cell_types import alias_cell_types, take_all_cell_classes
-from .. import test_simulation_control
-from .. import test_population
-from .. import test_populationview
-from .. import test_assembly
-#from .. import test_connectors
-from .. import test_projection
+#from .. import test_simulation_control
+#from .. import test_population
+#from .. import test_populationview
+#from .. import test_assembly
+from .. import test_connectors_parallel
+#from .. import test_projection
 
 try:
     import unittest2 as unittest
