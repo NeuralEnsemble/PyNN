54b5af57e2cfc1c34c65efc1beeeed0784e9d038
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2014-08-15 14:35:18 +0200

    Adding mentions of Python 3 to setup.py and to developers' guide.

--- setup.py
+++ setup.py
@@ -72,7 +72,11 @@ PyNN is a work in progress, but is already being used for several large-scale si
                    'License :: Other/Proprietary License',
                    'Natural Language :: English',
                    'Operating System :: OS Independent',
-                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
                    'Topic :: Scientific/Engineering'],
     cmdclass = {'build': build},
 )
