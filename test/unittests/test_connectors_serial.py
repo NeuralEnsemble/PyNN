NeuralEnsemble/PyNN

commit 5335d428009a8653d7f0c3c416f2a820176c41df

Corrects bug connector refusing to connect index 0 to index 0 if it was the unique connection for postsynaptic cell 0

src/connectors.py
@@ -165,13 +165,20 @@ class MapConnector(Connector):
             # `local`: boolean - does the post-synaptic neuron exist on this MPI node
             # `source_mask` - boolean numpy array, indicating which of the pre-synaptic neurons should be connected to,
             #                 or a single boolean, meaning connect to all/none of the pre-synaptic neurons
+            #                 It can also be an array of addresses
+            _proceed = False 
             if source_mask is True or source_mask.any():
+                _proceed = True
+            elif type(source_mask) == numpy.ndarray:
+                if len(source_mask) > 0:
+                    _proceed = True
+            if _proceed:
                 # Convert from boolean to integer mask, if necessary
                 if source_mask is True:
                     source_mask = numpy.arange(projection.pre.size, dtype=int)
                 elif source_mask.dtype == bool:
                     source_mask = source_mask.nonzero()[0]
-
+            
                 # Evaluate the lazy arrays containing the synaptic parameters
                 connection_parameters = {}
                 for name, map in parameter_space.items():
@@ -191,7 +198,7 @@ class MapConnector(Connector):
 
                 if local:
                     # Connect the neurons
-                    #logger.debug("Connecting to %d from %s" % (col, source_mask))
+                    logger.debug("Connecting to %d from %s" % (col, source_mask))
                     projection._convergent_connect(source_mask, col, **connection_parameters)
                     if self.callback:
                         self.callback(count/projection.post.local_size)

commit 86b04ced17cdae9d35b04c5e7260e5c11c40fab5

Adding test test_connect_weight_function_and_one_post_synaptic_neuron_not_connected. This test guarantees that we can have a postsynaptic neuron not connected. This test is to correct for a bug introduced in the commit 5335d428, more precisely to test against this bug.

test/unittests/test_connectors_serial.py
@@ -16,10 +16,10 @@ from numpy import nan
 import os
 import sys
 from numpy.testing import assert_array_equal, assert_array_almost_equal
-from .mocks import MockRNG, MockRNG2, MockRNG3
+from mocks import MockRNG, MockRNG2, MockRNG3
 import pyNN.mock as sim
 
-from .backends.registry import register_class, register
+from backends.registry import register_class, register
 
 orig_mpi_get_config = random.get_mpi_config
 
@@ -357,7 +357,26 @@ class TestFixedProbabilityConnector(unittest.TestCase):
                           (2, 4, 0.0, 0.123),
                           (3, 4, 0.0, 0.123),
                           ])
-        
+  
+    @register()
+    def test_connect_weight_function_and_one_post_synaptic_neuron_not_connected(self, sim=sim):  
+        C = connectors.FixedProbabilityConnector(p_connect=0.8,
+                                                 rng=MockRNG(delta=0.05))
+        syn = sim.StaticSynapse(weight=lambda d: 0.1*d)
+        prj = sim.Projection(self.p1, self.p2, C, syn)       
+        assert_array_almost_equal(prj.get(["weight", "delay"], format='array'),  
+                        numpy.array([
+                                        [[0., 0.1, 0.2, 0.3, nan],
+                                        [0.1,  0., 0.1, 0.2, nan],
+                                        [0.2, 0.1, 0.0, 0.1, nan],
+                                        [0.3, 0.2, 0.1, 0.0, nan]],
+                                    [[0.123, 0.123, 0.123, 0.123, nan],
+                                        [0.123, 0.123,   0.123, 0.123, nan],
+                                        [0.123, 0.123,   0.123, 0.123, nan],
+                                        [0.123, 0.123,   0.123, 0.123, nan]]
+                                        ]),
+                        9)
+  
     @register()
     def test_connect_with_weight_function(self, sim=sim):
         C = connectors.FixedProbabilityConnector(p_connect=0.85,