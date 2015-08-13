9dafd677edbe50876deeb50a023a3ed7e93eb48d
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2015-08-13 17:42:38 +0200

    Restore testing of Python versions 2.6, 3.3, 3.4

--- ci/install_nest.sh
+++ ci/install_nest.sh
@@ -2,28 +2,32 @@
 
 set -e  # stop execution in case of errors
 
-export NEST_VERSION="nest-2.6.0"
-pip install cython
-if [ ! -f "$HOME/$NEST_VERSION/configure" ]; then
-    wget http://www.nest-simulator.org/downloads/gplreleases/$NEST_VERSION.tar.gz -O $HOME/$NEST_VERSION.tar.gz;
-    pushd $HOME;
-    tar xzf $NEST_VERSION.tar.gz;
-    popd;
-else
-    echo 'Using cached version of NEST sources.';
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+
+    export NEST_VERSION="nest-2.6.0"
+    pip install cython
+    if [ ! -f "$HOME/$NEST_VERSION/configure" ]; then
+        wget http://www.nest-simulator.org/downloads/gplreleases/$NEST_VERSION.tar.gz -O $HOME/$NEST_VERSION.tar.gz;
+        pushd $HOME;
+        tar xzf $NEST_VERSION.tar.gz;
+        popd;
+    else
+        echo 'Using cached version of NEST sources.';
+    fi
+    mkdir -p $HOME/build/$NEST_VERSION
+    pushd $HOME/build/$NEST_VERSION
+    if [ ! -f "$HOME/build/$NEST_VERSION/config.log" ]; then
+        export VENV=`python -c "import sys; print sys.prefix"`;
+        $HOME/$NEST_VERSION/configure --with-mpi --prefix=$VENV;
+        make;
+    else
+        echo 'Using cached NEST build directory.';
+        echo "$HOME/$NEST_VERSION";
+        ls $HOME/$NEST_VERSION;
+        echo "$HOME/build/$NEST_VERSION";
+        ls $HOME/build/$NEST_VERSION;
+    fi
+    make install
+    popd
+
 fi
-mkdir -p $HOME/build/$NEST_VERSION
-pushd $HOME/build/$NEST_VERSION
-if [ ! -f "$HOME/build/$NEST_VERSION/config.log" ]; then
-    export VENV=`python -c "import sys; print sys.prefix"`;
-    $HOME/$NEST_VERSION/configure --with-mpi --prefix=$VENV;
-    make;
-else
-    echo 'Using cached NEST build directory.';
-    echo "$HOME/$NEST_VERSION";
-    ls $HOME/$NEST_VERSION;
-    echo "$HOME/build/$NEST_VERSION";
-    ls $HOME/build/$NEST_VERSION;
-fi
-make install
-popd
--- ci/test_script.sh
+++ ci/test_script.sh
@@ -3,4 +3,6 @@
 set -e  # stop execution in case of errors
 
 nosetests --with-coverage --cover-package=pyNN -e backends test/unittests
-nosetests --with-coverage --cover-package=pyNN -v test/system
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+    nosetests --with-coverage --cover-package=pyNN -v test/system;
+fi
