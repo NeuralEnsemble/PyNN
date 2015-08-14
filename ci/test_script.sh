dfc5b274e81eb42fae36e7b7a98aa3e2611e1b39
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2015-08-14 11:21:56 +0200

    Only run coverage with Python 2.7

--- ci/test_script.sh
+++ ci/test_script.sh
@@ -2,7 +2,9 @@
 
 set -e  # stop execution in case of errors
 
-nosetests --with-coverage --cover-package=pyNN -e backends test/unittests
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7_with_system_site_packages" ]]; then
+    nosetests --with-coverage --cover-package=pyNN -e backends test/unittests;
     nosetests --with-coverage --cover-package=pyNN -v test/system;
+else
+    nosetests -e backends test/unittests;
 fi
