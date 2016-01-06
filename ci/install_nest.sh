447ec30483a45e94704f1d25acb0218735738646
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2016-01-06 15:33:19 +0100

    Fix continuous integration install for NEST due to change in URL for downloading releases

--- ci/install_nest.sh
+++ ci/install_nest.sh
@@ -4,28 +4,29 @@ set -e  # stop execution in case of errors
 
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7_with_system_site_packages" ]]; then
 
-    export NEST_VERSION="nest-2.6.0"
+    export NEST_VERSION="2.6.0"
+    export NEST="nest-$NEST_VERSION"
     pip install cython
     if [ ! -f "$HOME/$NEST_VERSION/configure" ]; then
-        wget http://www.nest-simulator.org/downloads/gplreleases/$NEST_VERSION.tar.gz -O $HOME/$NEST_VERSION.tar.gz;
+        wget https://github.com/nest/nest-simulator/releases/download/v$NEST_VERSION/$NEST.tar.gz -O $HOME/$NEST.tar.gz;
         pushd $HOME;
-        tar xzf $NEST_VERSION.tar.gz;
+        tar xzf $NEST.tar.gz;
         popd;
     else
         echo 'Using cached version of NEST sources.';
     fi
-    mkdir -p $HOME/build/$NEST_VERSION
-    pushd $HOME/build/$NEST_VERSION
-    if [ ! -f "$HOME/build/$NEST_VERSION/config.log" ]; then
+    mkdir -p $HOME/build/$NEST
+    pushd $HOME/build/$NEST
+    if [ ! -f "$HOME/build/$NEST/config.log" ]; then
         export VENV=`python -c "import sys; print sys.prefix"`;
-        $HOME/$NEST_VERSION/configure --with-mpi --prefix=$VENV;
+        $HOME/$NEST/configure --with-mpi --prefix=$VENV;
         make;
     else
         echo 'Using cached NEST build directory.';
-        echo "$HOME/$NEST_VERSION";
-        ls $HOME/$NEST_VERSION;
-        echo "$HOME/build/$NEST_VERSION";
-        ls $HOME/build/$NEST_VERSION;
+        echo "$HOME/$NEST";
+        ls $HOME/$NEST;
+        echo "$HOME/build/$NEST";
+        ls $HOME/build/$NEST;
     fi
     make install
     popd
