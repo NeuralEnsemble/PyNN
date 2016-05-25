3a9fe206fe8baa17b5e3735c5b03db105e6bec01
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2016-05-25 23:40:53 +0200

    Updated version number to 0.8.1

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ class build(_build):
 
 setup(
     name="PyNN",
-    version="0.8.1dev",
+    version="0.8.1",
     packages=['pyNN', 'pyNN.nest', 'pyNN.neuron',
                 'pyNN.brian', 'pyNN.common', 'pyNN.mock',
                 'pyNN.recording', 'pyNN.standardmodels', 'pyNN.descriptions',
