41006a44e9edcdb4b90288da0e1eee1152f5fdd7
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2017-05-29 16:59:31 +0200

    Changes to allow running nosetests for specific tests from system.scenarios

--- test/system/scenarios/registry.py
+++ test/system/scenarios/registry.py
@@ -1,10 +1,11 @@
+from testconfig import config
 
 registry = []
 
 
 def register(exclude=[]):
     def inner_register(scenario):
-        if scenario not in registry:
+        if scenario not in registry and not ('testName' in config and not scenario.__name__ == config['testName']):
             scenario.exclude = exclude
             registry.append(scenario)
         return scenario
