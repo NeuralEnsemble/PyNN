8f5aa41ff6a0a308c2349399ea2597a4db89c7da
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2017-06-01 16:06:29 +0200

    Adds nose-testconfig to ci/install.sh
    (part of PR 479)

--- ci/install.sh
+++ ci/install.sh
@@ -5,6 +5,7 @@ set -e  # stop execution in case of errors
 sudo apt-get install -qq python-numpy python3-numpy python-scipy python3-scipy libgsl0-dev  openmpi-bin libopenmpi-dev
 pip install -r requirements.txt
 pip install coverage coveralls
+pip install nose-testconfig
 source ci/install_brian.sh
 source ci/install_nest.sh
 source ci/install_neuron.sh
