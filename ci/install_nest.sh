72f92e1634d0e2106ff75e2a2389ebdd83dfd3fc
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2017-07-06 10:33:20 +0200

    Updates NEST install script for Travis - diff between master and nest-dev

--- ci/install_nest.sh
+++ ci/install_nest.sh
@@ -8,7 +8,7 @@ if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ] || [ "$TRAVIS_PYTHON_VERSION" == "3.5" 
     export NEST_VERSION="master"
     #export NEST_VERSION="2.12.0"
 
-    export NEST="nest-$NEST_VERSION"
+    export NEST="nest-simulator-$NEST_VERSION"
     pip install cython==0.23.4
 
     if [ "$NEST_VERSION" = "master" ]; then
@@ -25,8 +25,6 @@ if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ] || [ "$TRAVIS_PYTHON_VERSION" == "3.5" 
     mkdir -p $HOME/build/$NEST
     pushd $HOME/build/$NEST
     export VENV=`python -c "import sys; print(sys.prefix)"`;
-    echo "VENV = $VENV";
-    echo "PATH = $PATH";
     ln -s /opt/python/2.7.13/lib/libpython2.7.so $VENV/lib/libpython2.7.so;
     ln -s /opt/python/3.5.2/lib/libpython3.5m.so $VENV/lib/libpython3.5.so;
     export PYTHON_INCLUDE_DIR=$VENV/include/python${TRAVIS_PYTHON_VERSION}
@@ -37,8 +35,6 @@ if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ] || [ "$TRAVIS_PYTHON_VERSION" == "3.5" 
     cmake --version;
     cmake -DCMAKE_INSTALL_PREFIX=$VENV \
           -Dwith-mpi=ON  \
-          -DPYTHON_EXECUTABLE=$VENV/bin/python \
-          -DCYTHON_EXECUTABLE=$VENV/bin/cython \
           -DPYTHON_LIBRARY=$VENV/lib/libpython${TRAVIS_PYTHON_VERSION}.so \
           -DPYTHON_INCLUDE_DIR=${PYTHON_INCLUDE_DIR} \
           $HOME/$NEST;
