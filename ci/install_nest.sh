c5ca8235aeeb80a74f91c7b5013c99bbe5dd4bea
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2017-11-21 13:19:13 +0100

    Travis test against NEST 2.14.0

--- ci/install_nest.sh
+++ ci/install_nest.sh
@@ -5,15 +5,16 @@ set -e  # stop execution in case of errors
 if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ] || [ "$TRAVIS_PYTHON_VERSION" == "3.5" ]; then
     echo -e "\n========== Installing NEST ==========\n"
     # Specify which version of NEST to install
-    export NEST_VERSION="master"
-    #export NEST_VERSION="2.12.0"
+    #export NEST_VERSION="master"
+    export NEST_VERSION="2.14.0"
 
-    export NEST="nest-simulator-$NEST_VERSION"
     pip install cython==0.23.4
 
     if [ "$NEST_VERSION" = "master" ]; then
+      export NEST="nest-simulator-$NEST_VERSION"
       wget https://github.com/nest/nest-simulator/archive/$NEST_VERSION.tar.gz -O $HOME/$NEST.tar.gz;
     else
+      export NEST="nest-$NEST_VERSION"
       wget https://github.com/nest/nest-simulator/releases/download/v$NEST_VERSION/nest-$NEST_VERSION.tar.gz -O $HOME/$NEST.tar.gz
     fi
 
@@ -35,6 +36,8 @@ if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ] || [ "$TRAVIS_PYTHON_VERSION" == "3.5" 
     cmake --version;
     cmake -DCMAKE_INSTALL_PREFIX=$VENV \
           -Dwith-mpi=ON  \
+          -DPYTHON_EXECUTABLE=$VENV/bin/python \
+          -DCYTHON_EXECUTABLE=$VENV/bin/cython \
           -DPYTHON_LIBRARY=$VENV/lib/libpython${TRAVIS_PYTHON_VERSION}.so \
           -DPYTHON_INCLUDE_DIR=${PYTHON_INCLUDE_DIR} \
           $HOME/$NEST;
