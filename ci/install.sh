abf28d41541b8ee524b1c5abb260366002ab91b4
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-06-14 00:14:29 +0100

    Install python dev for both py2 and py3

--- ci/install.sh
+++ ci/install.sh
@@ -2,11 +2,14 @@
 
 set -e  # stop execution in case of errors
 
-sudo apt-get update && sudo apt-get install -qq python-numpy python3-numpy python-scipy python3-scipy libgsl0-dev  openmpi-bin libopenmpi-dev
+sudo apt-get update && sudo apt-get install -qq python-numpy python3-numpy python-scipy python3-scipy libgsl0-dev  openmpi-bin libopenmpi-dev python-all-dev build-essential python-all-dev python3-all-dev
+
 pip install -r requirements.txt
 pip install coverage coveralls
 pip install nose-testconfig
+
 source ci/install_brian.sh
 source ci/install_nest.sh
 source ci/install_neuron.sh
+
 python setup.py install
