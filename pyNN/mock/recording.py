cd7f80eeef8191d9e2dcab505866cfee28bce8c0
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-06-14 18:10:56 +0100

    Replace builtin named variable with a variant

--- pyNN/mock/recording.py
+++ pyNN/mock/recording.py
@@ -9,19 +9,33 @@ class Recorder(recording.Recorder):
     def _record(self, variable, new_ids, sampling_interval=None):
         pass
 
-    def _get_spiketimes(self, id):
-        return numpy.array([id, id + 5], dtype=float) % self._simulator.state.t
+    def _get_spiketimes(self, id_):
+        # common.Recorder._get_current_segment creates id_ as a set, so we must
+        # cast it to numpy.array if we want to add a scalar to each element
+        # below
+        id_ = numpy.array(id_)
+        data = numpy.array([id_, id_ + 5], dtype=float) % self._simulator.state.t
+
+        # _get_spiketimes is expected to return a dict
+        retval = {x[0]: x[1] for x in data}
+        print(retval)
+        return retval
 
     def _get_all_signals(self, variable, ids, clear=False):
-        # assuming not using cvode, otherwise need to get times as well and use IrregularlySampledAnalogSignal
-        n_samples = int(round(self._simulator.state.t / self._simulator.state.dt)) + 1
-        return numpy.vstack((numpy.random.uniform(size=n_samples) for id in ids)).T
+        # assuming not using cvode, otherwise need to get times as well and use
+        # IrregularlySampledAnalogSignal
+        n_samples = (int(round(self._simulator.state.t /
+                               self._simulator.state.dt)) + 1)
+        return (
+            numpy.vstack((
+                numpy.random.uniform(size=n_samples) for id_ in ids)).T
+        )
 
     def _local_count(self, variable, filter_ids=None):
         N = {}
         if variable == 'spikes':
-            for id in self.filter_recorded(variable, filter_ids):
-                N[int(id)] = 2
+            for id_ in self.filter_recorded(variable, filter_ids):
+                N[int(id_)] = 2
         else:
             raise Exception("Only implemented for spikes")
         return N
