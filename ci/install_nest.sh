22bad06446d6299df7318eb5ebaa676a954121f1
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-06-14 23:42:12 +0100

    Update python version for nest

--- ci/install_nest.sh
+++ ci/install_nest.sh
@@ -8,7 +8,7 @@ if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ] || [ "$TRAVIS_PYTHON_VERSION" == "3.5" 
     #export NEST_VERSION="master"
     export NEST_VERSION="2.14.0"
 
-    pip install cython==0.23.4
+    pip install cython
 
     if [ "$NEST_VERSION" = "master" ]; then
       export NEST="nest-simulator-$NEST_VERSION"
@@ -26,8 +26,8 @@ if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ] || [ "$TRAVIS_PYTHON_VERSION" == "3.5" 
     mkdir -p $HOME/build/$NEST
     pushd $HOME/build/$NEST
     export VENV=`python -c "import sys; print(sys.prefix)"`;
-    ln -s /opt/python/2.7.14/lib/libpython2.7.so $VENV/lib/libpython2.7.so;
-    ln -s /opt/python/3.5.4/lib/libpython3.5m.so $VENV/lib/libpython3.5.so;
+    ln -s -v /opt/python/2.7.14/lib/libpython2.7.so $VENV/lib/libpython2.7.so;
+    ln -s -v /opt/python/3.5.5/lib/libpython3.5m.so $VENV/lib/libpython3.5.so;
     export PYTHON_INCLUDE_DIR=$VENV/include/python${TRAVIS_PYTHON_VERSION}
     if [ "$TRAVIS_PYTHON_VERSION" == "3.5" ]; then
         export PYTHON_INCLUDE_DIR=${PYTHON_INCLUDE_DIR}m;
