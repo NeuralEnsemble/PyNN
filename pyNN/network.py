a36629c963d7156c23fb878c5e77c63301260a60
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2019-03-28 11:57:52 +0100

    In sonata module, fully implement filtering in node sets based on node population attributes; support step current injection

--- pyNN/network.py
+++ pyNN/network.py
@@ -3,6 +3,8 @@
 
 """
 
+import sys
+import inspect
 from itertools import chain
 try:
     basestring
@@ -22,6 +24,23 @@ class Network(object):
         self.views = set([])
         self.assemblies = set([])
         self.projections = set([])
+        self.add(*components)
+
+    @property
+    def sim(self):
+        """Figure out which PyNN backend module this Network is using."""
+        # we assume there is only one. Could be mixed if using multiple simulators
+        # at once.
+        populations_module = inspect.getmodule(list(self.populations)[0].__class__)
+        return sys.modules[".".join(populations_module.__name__.split(".")[:-1])]
+
+    def count_neurons(self):
+        return sum(population.size for population in chain(self.populations))
+
+    def count_connections(self):
+        return sum(projection.size() for projection in chain(self.projections))
+
+    def add(self, *components):
         for component in components:
             if isinstance(component, Population):
                 self.populations.add(component)
@@ -37,17 +56,23 @@ class Network(object):
             else:
                 raise TypeError()
 
-    def count_neurons(self):
-        return sum(population.size for population in chain(self.populations))
-
-    def count_connections(self):
-        return sum(projection.size() for projection in chain(self.projections))
-
     def get_component(self, label):
         for obj in chain(self.populations, self.views, self.assemblies, self.projections):
             if obj.label == label:
                 return obj
         return None
+
+    def filter(self, cell_types=None):
+        """Return an Assembly of all components that have a cell type in the list"""
+        if cell_types is None:
+            raise NotImplementedError()
+        else:
+            if cell_types == "all":
+                return self.sim.Assembly(*(pop for pop in self.populations
+                                           if pop.celltype.injectable))  # or could use len(receptor_types) > 0
+            else:
+                return self.sim.Assembly(*(pop for pop in self.populations
+                                           if pop.celltype.__class__ in cell_types))
 
     def record(self, variables, to_file=None, sampling_interval=None, include_spike_source=True):
         for obj in chain(self.populations, self.assemblies):
