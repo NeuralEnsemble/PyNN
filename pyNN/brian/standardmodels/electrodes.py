cf7f973f5b59b4b1302d2987c172b0a6bfcb2259
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2019-04-25 18:01:23 +0200

    Fix simultaneous current sources in Brian

--- pyNN/brian/standardmodels/electrodes.py
+++ pyNN/brian/standardmodels/electrodes.py
@@ -103,16 +103,19 @@ class BrianCurrentSource(StandardCurrentSource):
         if self.running and abs(simulator.state.t - self.times[self.i] * 1e3) < (simulator.state.dt/2.0):
             for cell, idx in zip(self.cell_list, self.indices):
                 if not self._is_playable:
-                    cell.parent.brian_group.i_inj[idx] = self.amplitudes[self.i] * ampere
+                    cell.parent.brian_group.i_inj[idx] += self.amplitudes[self.i] - self.prev_amp #* ampere
+                    self.prev_amp = self.amplitudes[self.i]
                 else:
-                    cell.parent.brian_group.i_inj[idx] = self._compute(self.times[self.i]) * ampere
+                    amp_val = self._compute(self.times[self.i])
+                    cell.parent.brian_group.i_inj[idx] += amp_val - self.prev_amp
+                    self.prev_amp = amp_val #* ampere
             self.i += 1
             if self.i >= len(self.times):
                 self.running = False
                 if self._is_playable:
                     # ensure that currents are set to 0 after t_stop
                     for cell, idx in zip(self.cell_list, self.indices):
-                        cell.parent.brian_group.i_inj[idx] = 0
+                        cell.parent.brian_group.i_inj[idx] -= self.prev_amp
 
     def record(self):
         self.i_state_monitor = brian.StateMonitor(self.cell_list[0].parent.brian_group[self.indices[0]], 'i_inj', record=0, when='start')
@@ -140,6 +143,10 @@ class StepCurrentSource(BrianCurrentSource, electrodes.StepCurrentSource):
     _is_computed = False
     _is_playable = False
 
+    def __init__(self, **parameters):
+        BrianCurrentSource.__init__(self, **parameters)
+        self.prev_amp = 0.0
+
 
 class ACSource(BrianCurrentSource, electrodes.ACSource):
     __doc__ = electrodes.ACSource.__doc__
@@ -157,6 +164,7 @@ class ACSource(BrianCurrentSource, electrodes.ACSource):
 
     def __init__(self, **parameters):
         BrianCurrentSource.__init__(self, **parameters)
+        self.prev_amp = 0.0
         self._generate()
 
     def _generate(self):
@@ -182,6 +190,7 @@ class DCSource(BrianCurrentSource, electrodes.DCSource):
 
     def __init__(self, **parameters):
         BrianCurrentSource.__init__(self, **parameters)
+        self.prev_amp = 0.0
         self._generate()
 
     def _generate(self):
@@ -214,6 +223,7 @@ class NoisyCurrentSource(BrianCurrentSource, electrodes.NoisyCurrentSource):
 
     def __init__(self, **parameters):
         BrianCurrentSource.__init__(self, **parameters)
+        self.prev_amp = 0.0
         self._generate()
 
     def _generate(self):
