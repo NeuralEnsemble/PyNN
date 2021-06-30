a8c19d3a2e44f2d496dad34d91dd40cdab3d1c22
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2021-06-30 19:13:19 +0200

    Missed a few lines in Travis config for Python 3.8

--- ci/install_brian.sh
+++ ci/install_brian.sh
@@ -2,7 +2,7 @@
 
 set -e  # stop execution in case of errors
 
-if [ "$TRAVIS_PYTHON_VERSION" == "3.7" ]; then
+if [ "$TRAVIS_PYTHON_VERSION" == "3.8" ]; then
     echo -e "\n========== Installing Brian 2 ==========\n"
     pip install cython;
     pip install brian2;
