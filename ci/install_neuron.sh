94053a32b11b3d36da394dacf68e4f66f3323f23
Andrew Davison <andrew.davison@cnrs.fr>
2021-10-26 16:32:22 +0200

    Re-enable caching of NEURON build

--- ci/install_neuron.sh
+++ ci/install_neuron.sh
@@ -5,14 +5,21 @@ set -e  # stop execution in case of errors
 if [ "$TRAVIS_PYTHON_VERSION" == "3.9" ]; then
     echo -e "\n========== Installing NEURON ==========\n"
     export NRN_VERSION="nrn-8.0.0"
-    git clone https://github.com/neuronsimulator/nrn -b 8.0.0 $HOME/$NRN_VERSION
 
-    mkdir -p $HOME/$NRN_VERSION/build
-    pushd $HOME/$NRN_VERSION/build
-    export VENV=`python -c "import sys; print(sys.prefix)"`;
+    if [ ! -f "$HOME/$NRN_VERSION/build/config.log" ]; then
 
-    cmake .. -DNRN_ENABLE_INTERVIEWS=OFF -DNRN_ENABLE_MPI=ON -DNRN_ENABLE_RX3D=OFF -DCMAKE_INSTALL_PREFIX=$VENV;
-    cmake --build . --target install
+        git clone https://github.com/neuronsimulator/nrn -b 8.0.0 $HOME/$NRN_VERSION
+
+        mkdir -p $HOME/$NRN_VERSION/build
+        pushd $HOME/$NRN_VERSION/build
+        export VENV=`python -c "import sys; print(sys.prefix)"`
+
+        cmake .. -DNRN_ENABLE_INTERVIEWS=OFF -DNRN_ENABLE_MPI=ON -DNRN_ENABLE_RX3D=OFF -DCMAKE_INSTALL_PREFIX=$VENV
+        cmake --build . --target install
+        ls
+    else
+        echo 'Using cached NEURON build directory.'
+    fi
 
     pip install nrnutils  # must be installed after NEURON
 
