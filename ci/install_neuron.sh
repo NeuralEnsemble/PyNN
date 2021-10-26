3169b0bf0ffe7f1482288fdd58c664e0342504d2
Andrew Davison <andrew.davison@cnrs.fr>
2021-10-26 18:09:24 +0200

    make sure `neuron` package is installed in site-packages
    (see https://neuronsimulator.github.io/nrn/cmake_doc/options.html#python-options)

--- ci/install_neuron.sh
+++ ci/install_neuron.sh
@@ -5,21 +5,19 @@ set -e  # stop execution in case of errors
 if [ "$TRAVIS_PYTHON_VERSION" == "3.9" ]; then
     echo -e "\n========== Installing NEURON ==========\n"
     export NRN_VERSION="nrn-8.0.0"
+    export VENV=`python -c "import sys; print(sys.prefix)"`
 
-    if [ ! -f "$HOME/$NRN_VERSION/build/config.log" ]; then
-
+    if [ ! -f "$HOME/$NRN_VERSION/build/CMakeCache.txt" ]; then
+        echo 'Cloning NEURON sources from GitHub'
         git clone https://github.com/neuronsimulator/nrn -b 8.0.0 $HOME/$NRN_VERSION
-
         mkdir -p $HOME/$NRN_VERSION/build
-        pushd $HOME/$NRN_VERSION/build
-        export VENV=`python -c "import sys; print(sys.prefix)"`
-
-        cmake .. -DNRN_ENABLE_INTERVIEWS=OFF -DNRN_ENABLE_MPI=ON -DNRN_ENABLE_RX3D=OFF -DCMAKE_INSTALL_PREFIX=$VENV
-        cmake --build . --target install
-        ls
-    else
+    else:
         echo 'Using cached NEURON build directory.'
     fi
+    pushd $HOME/$NRN_VERSION/build
+
+    cmake .. -DNRN_ENABLE_INTERVIEWS=OFF -DNRN_ENABLE_MPI=ON -DNRN_ENABLE_RX3D=OFF -DCMAKE_INSTALL_PREFIX=$VENV -DNRN_MODULE_INSTALL_OPTIONS=""
+    cmake --build . --target install
 
     pip install nrnutils  # must be installed after NEURON
 
--- ci/test_script.sh
+++ ci/test_script.sh
@@ -3,7 +3,7 @@
 set -e  # stop execution in case of errors
 
 if [ "$TRAVIS_PYTHON_VERSION" == "3.9" ]; then
-    python setup.py nosetests --with-coverage --cover-package=pyNN -v --tests=test;
+    python setup.py nosetests --verbose --with-coverage --cover-package=pyNN --tests=test;
 else
-    python setup.py nosetests -e backends -v --tests=test/unittests;
+    python setup.py nosetests --verbose -e backends --tests=test/unittests
 fi
