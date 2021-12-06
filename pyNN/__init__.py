5a02421cb82bc50c9f773f6e1c497351eea4a358
Andrew Davison <andrew.davison@cnrs.fr>
2021-12-06 17:39:44 +0100

    increment version number to 0.10.1.dev

--- pyNN/__init__.py
+++ pyNN/__init__.py
@@ -69,7 +69,7 @@ Other modules:
 :license: CeCILL, see LICENSE for details.
 """
 
-__version__ = '0.10.0'
+__version__ = '0.10.1.dev'
 __all__ = ["common", "random", "nest", "neuron", "brian2",
            "recording", "errors", "space", "descriptions",
            "standardmodels", "parameters", "core", "serialization"]
--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ class build(_build):
 
 setup(
     name="PyNN",
-    version="0.10.0",
+    version="0.10.1.dev",
     packages=['pyNN', 'pyNN.nest', 'pyNN.neuron',
               'pyNN.brian2', 'pyNN.common', 'pyNN.mock', 'pyNN.neuroml',
               'pyNN.recording', 'pyNN.standardmodels', 'pyNN.descriptions',
