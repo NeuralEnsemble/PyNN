b6589e1b3ea50e0ae4b421bb9abf9ea29639c3f0
Andrew Davison <andrew.davison@cnrs.fr>
2022-04-15 13:58:36 +0200

    Adapt pyNN.nest backend to use neo.SpikeTrainList

--- pyNN/nest/recording.py
+++ pyNN/nest/recording.py
@@ -51,10 +51,9 @@ class RecordingDevice(object):
         assert not self._connected
         self._all_ids = self._all_ids.union(new_ids)
 
-    def get_data(self, variable, desired_ids, clear=False):
+    def _get_data_arrays(self, variable, clear=False):
         """
-        Return recorded data as a dictionary containing one numpy array for
-        each neuron, ids as keys.
+        Return recorded data as pair of NumPy arrays: ids and values.
         """
         scale_factor = SCALE_FACTORS.get(variable, 1)
         nest_variable = VARIABLE_MAP.get(variable, variable)
@@ -64,7 +63,8 @@ class RecordingDevice(object):
         if variable == "times":
             values = times
         else:
-            # I'm hoping numpy optimises for the case where scale_factor = 1, otherwise should avoid this multiplication in that case
+            # I'm hoping numpy optimises for the case where scale_factor = 1, 
+            # otherwise should avoid this multiplication in that case
             values = events[nest_variable] * scale_factor
 
         valid_times_index = times <= simulator.state.t
@@ -84,6 +84,14 @@ class RecordingDevice(object):
         else:
             ids = ids[valid_times_index]
             values = values[valid_times_index]
+        return ids, values
+
+    def get_data(self, variable, desired_ids, clear=False):
+        """
+        Return recorded data as a dictionary containing one numpy array for
+        each neuron, ids as keys.
+        """
+        ids, values = self._get_data_arrays(variable, clear=clear)
 
         data = {}
         recorded_ids = set(ids)
@@ -149,7 +157,11 @@ class SpikeDetector(RecordingDevice):
 
         Equivalent to `get_data('times', desired_ids)`
         """
-        return self.get_data('times', desired_ids, clear=clear)
+        id_array, times_array = self._get_data_arrays("times", clear=clear)
+        recorded_ids = np.unique(id_array)
+        desired_and_existing_ids = np.intersect1d(recorded_ids, np.array(desired_ids))
+        mask = np.in1d(id_array, desired_and_existing_ids)
+        return id_array[mask], times_array[mask]
 
     def get_spike_counts(self, desired_ids):
         events = nest.GetStatus(self.device, 'events')[0]
@@ -258,7 +270,6 @@ class Recorder(recording.Recorder):
         self._spike_detector = SpikeDetector()
 
     def _get_spiketimes(self, ids, clear=False):
-        # hugely inefficient - to be optimized later
         return self._spike_detector.get_spiketimes(ids, clear=clear)
 
     def _get_all_signals(self, variable, ids, clear=False):
