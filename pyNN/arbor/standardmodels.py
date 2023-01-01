92f4484c62bd7940ae1698c39d190419ff9db82d
lungsi <neuralgraphs@gmail.com>
2023-01-01 19:19:48 +0100

    commits for arbor branch

---
+++ pyNN/arbor/standardmodels.py
@@ -0,0 +1,345 @@
+# encoding: utf-8
+"""
+Standard cells for the mock module.
+
+:copyright: Copyright 2006-2020 by the PyNN team, see AUTHORS.
+:license: CeCILL, see LICENSE for details.
+"""
+
+from pyNN.standardmodels import cells, synapses, electrodes, build_translations, StandardCurrentSource
+from .simulator import state
+import logging
+
+logger = logging.getLogger("PyNN")
+
+
+class IF_curr_alpha(cells.IF_curr_alpha):
+    __doc__ = cells.IF_curr_alpha.__doc__
+
+    translations = build_translations(  # should add some computed/scaled parameters
+        ('tau_m',      'TAU_M'),
+        ('cm',         'CM'),
+        ('v_rest',     'V_REST'),
+        ('v_thresh',   'V_THRESH'),
+        ('v_reset',    'V_RESET'),
+        ('tau_refrac', 'TAU_REFRAC'),
+        ('i_offset',   'I_OFFSET'),
+        ('tau_syn_E',  'TAU_SYN_E'),
+        ('tau_syn_I',  'TAU_SYN_I'),
+    )
+
+
+class IF_curr_exp(cells.IF_curr_exp):
+    __doc__ = cells.IF_curr_exp.__doc__
+
+    translations = build_translations(  # should add some computed/scaled parameters
+        ('tau_m',      'TAU_M'),
+        ('cm',         'CM'),
+        ('v_rest',     'V_REST'),
+        ('v_thresh',   'V_THRESH'),
+        ('v_reset',    'V_RESET'),
+        ('tau_refrac', 'T_REFRAC'),
+        ('i_offset',   'I_OFFSET'),
+        ('tau_syn_E',  'TAU_SYN_E'),
+        ('tau_syn_I',  'TAU_SYN_I'),
+    )
+
+
+class IF_cond_alpha(cells.IF_cond_alpha):
+    __doc__ = cells.IF_cond_alpha.__doc__
+
+    translations = build_translations(
+        ('tau_m',      'TAU_M'),
+        ('cm',         'CM'),
+        ('v_rest',     'V_REST'),
+        ('v_thresh',   'V_THRESH'),
+        ('v_reset',    'V_RESET'),
+        ('tau_refrac', 'TAU_REFRAC'),
+        ('i_offset',   'I_OFFSET'),
+        ('tau_syn_E',  'TAU_SYN_E'),
+        ('tau_syn_I',  'TAU_SYN_I'),
+        ('e_rev_E',    'E_REV_E'),
+        ('e_rev_I',    'E_REV_I')
+    )
+
+
+class IF_cond_exp(cells.IF_cond_exp):
+    __doc__ = cells.IF_cond_exp.__doc__
+
+    translations = build_translations(
+        ('tau_m',      'TAU_M'),
+        ('cm',         'CM'),
+        ('v_rest',     'V_REST'),
+        ('v_thresh',   'V_THRESH'),
+        ('v_reset',    'V_RESET'),
+        ('tau_refrac', 'TAU_REFRAC'),
+        ('i_offset',   'I_OFFSET'),
+        ('tau_syn_E',  'TAU_SYN_E'),
+        ('tau_syn_I',  'TAU_SYN_I'),
+        ('e_rev_E',    'E_REV_E'),
+        ('e_rev_I',    'E_REV_I')
+    )
+
+
+class IF_facets_hardware1(cells.IF_facets_hardware1):
+    __doc__ = cells.IF_facets_hardware1.__doc__
+
+
+class HH_cond_exp(cells.HH_cond_exp):
+    __doc__ = cells.HH_cond_exp.__doc__
+
+    translations = build_translations(
+        ('gbar_Na',    'GBAR_NA'),
+        ('gbar_K',     'GBAR_K'),
+        ('g_leak',     'G_LEAK'),
+        ('cm',         'CM'),
+        ('v_offset',   'V_OFFSET'),
+        ('e_rev_Na',   'E_REV_NA'),
+        ('e_rev_K',    'E_REV_K'),
+        ('e_rev_leak', 'E_REV_LEAK'),
+        ('e_rev_E',    'E_REV_E'),
+        ('e_rev_I',    'E_REV_I'),
+        ('tau_syn_E',  'TAU_SYN_E'),
+        ('tau_syn_I',  'TAU_SYN_I'),
+        ('i_offset',   'I_OFFSET'),
+    )
+
+
+class IF_cond_exp_gsfa_grr(cells.IF_cond_exp_gsfa_grr):
+    __doc__ = cells.IF_cond_exp_gsfa_grr.__doc__
+
+
+class SpikeSourcePoisson(cells.SpikeSourcePoisson):
+    __doc__ = cells.SpikeSourcePoisson.__doc__
+
+    translations = build_translations(
+        ('start',    'START'),
+        ('rate',     'INTERVAL',  "1000.0/rate",  "1000.0/INTERVAL"),
+        ('duration', 'DURATION'),
+    )
+
+
+class SpikeSourceArray(cells.SpikeSourceArray):
+    __doc__ = cells.SpikeSourceArray.__doc__
+
+    translations = build_translations(
+        ('spike_times', 'SPIKE_TIMES'),
+    )
+
+
+class EIF_cond_alpha_isfa_ista(cells.EIF_cond_alpha_isfa_ista):
+    __doc__ = cells.EIF_cond_alpha_isfa_ista.__doc__
+
+    translations = build_translations(
+        ('cm',         'CM'),
+        ('tau_refrac', 'TAU_REFRAC'),
+        ('v_spike',    'V_SPIKE'),
+        ('v_reset',    'V_RESET'),
+        ('v_rest',     'V_REST'),
+        ('tau_m',      'TAU_M'),
+        ('i_offset',   'I_OFFSET'),
+        ('a',          'A'),
+        ('b',          'B'),
+        ('delta_T',    'DELTA_T'),
+        ('tau_w',      'TAU_W'),
+        ('v_thresh',   'V_THRESH'),
+        ('e_rev_E',    'E_REV_E'),
+        ('tau_syn_E',  'TAU_SYN_E'),
+        ('e_rev_I',    'E_REV_I'),
+        ('tau_syn_I',  'TAU_SYN_I'),
+    )
+
+
+class EIF_cond_exp_isfa_ista(cells.EIF_cond_exp_isfa_ista):
+    __doc__ = cells.EIF_cond_exp_isfa_ista.__doc__
+
+    translations = build_translations(
+        ('cm',         'CM'),
+        ('tau_refrac', 'TAU_REFRAC'),
+        ('v_spike',    'V_SPIKE'),
+        ('v_reset',    'V_RESET'),
+        ('v_rest',     'V_REST'),
+        ('tau_m',      'TAU_M'),
+        ('i_offset',   'I_OFFSET'),
+        ('a',          'A'),
+        ('b',          'B'),
+        ('delta_T',    'DELTA_T'),
+        ('tau_w',      'TAU_W'),
+        ('v_thresh',   'V_THRESH'),
+        ('e_rev_E',    'E_REV_E'),
+        ('tau_syn_E',  'TAU_SYN_E'),
+        ('e_rev_I',    'E_REV_I'),
+        ('tau_syn_I',  'TAU_SYN_I'),
+    )
+
+
+class Izhikevich(cells.Izhikevich):
+    __doc__ = cells.Izhikevich.__doc__
+
+    translations = build_translations(
+        ('a',        'a'),
+        ('b',        'b'),
+        ('c',        'c'),
+        ('d',        'd'),
+        ('i_offset', 'I_e'),
+    )
+    standard_receptor_type = True
+    receptor_scale = 1e-3  # synaptic weight is in mV, so need to undo usual weight scaling
+
+
+class MockCurrentSource(object):
+
+    def inject_into(self, cells):
+        __doc__ = StandardCurrentSource.inject_into.__doc__
+        pass
+
+
+class DCSource(MockCurrentSource, electrodes.DCSource):
+    __doc__ = electrodes.DCSource.__doc__
+
+    translations = build_translations(
+        ('amplitude',  'amplitude'),
+        ('start',      'start'),
+        ('stop',       'stop')
+    )
+
+
+class StepCurrentSource(MockCurrentSource, electrodes.StepCurrentSource):
+    __doc__ = electrodes.StepCurrentSource.__doc__
+
+    translations = build_translations(
+        ('amplitudes',  'amplitudes'),
+        ('times',       'times')
+    )
+
+
+class ACSource(MockCurrentSource, electrodes.ACSource):
+    __doc__ = electrodes.ACSource.__doc__
+
+    translations = build_translations(
+        ('amplitude',  'amplitude'),
+        ('start',      'start'),
+        ('stop',       'stop'),
+        ('frequency',  'frequency'),
+        ('offset',     'offset'),
+        ('phase',      'phase')
+    )
+
+
+class NoisyCurrentSource(MockCurrentSource, electrodes.NoisyCurrentSource):
+
+    translations = build_translations(
+        ('mean',  'mean'),
+        ('start', 'start'),
+        ('stop',  'stop'),
+        ('stdev', 'stdev'),
+        ('dt',    'dt')
+    )
+
+
+class StaticSynapse(synapses.StaticSynapse):
+    __doc__ = synapses.StaticSynapse.__doc__
+    translations = build_translations(
+        ('weight', 'WEIGHT'),
+        ('delay', 'DELAY'),
+    )
+
+    def _get_minimum_delay(self):
+        d = state.min_delay
+        if d == 'auto':
+            d = state.dt
+        return d
+
+
+class TsodyksMarkramSynapse(synapses.TsodyksMarkramSynapse):
+    __doc__ = synapses.TsodyksMarkramSynapse.__doc__
+
+    translations = build_translations(
+        ('weight', 'WEIGHT'),
+        ('delay', 'DELAY'),
+        ('U', 'UU'),
+        ('tau_rec', 'TAU_REC'),
+        ('tau_facil', 'TAU_FACIL'),
+        ('u0', 'U0'),
+        ('x0', 'X'),
+        ('y0', 'Y')
+    )
+
+    def _get_minimum_delay(self):
+        d = state.min_delay
+        if d == 'auto':
+            d = state.dt
+        return d
+
+
+class STDPMechanism(synapses.STDPMechanism):
+    __doc__ = synapses.STDPMechanism.__doc__
+
+    base_translations = build_translations(
+        ('weight', 'WEIGHT'),
+        ('delay', 'DELAY'),
+        ('dendritic_delay_fraction', 'dendritic_delay_fraction')
+    )
+
+    def _get_minimum_delay(self):
+        d = state.min_delay
+        if d == 'auto':
+            d = state.dt
+        return d
+
+
+class AdditiveWeightDependence(synapses.AdditiveWeightDependence):
+    __doc__ = synapses.AdditiveWeightDependence.__doc__
+
+    translations = build_translations(
+        ('w_max',     'wmax'),
+        ('w_min',     'wmin'),
+        ('A_plus',    'aLTP'),
+        ('A_minus',   'aLTD'),
+    )
+
+
+class MultiplicativeWeightDependence(synapses.MultiplicativeWeightDependence):
+    __doc__ = synapses.MultiplicativeWeightDependence.__doc__
+
+    translations = build_translations(
+        ('w_max',     'wmax'),
+        ('w_min',     'wmin'),
+        ('A_plus',    'aLTP'),
+        ('A_minus',   'aLTD'),
+    )
+
+
+class AdditivePotentiationMultiplicativeDepression(synapses.AdditivePotentiationMultiplicativeDepression):
+    __doc__ = synapses.AdditivePotentiationMultiplicativeDepression.__doc__
+
+    translations = build_translations(
+        ('w_max',     'wmax'),
+        ('w_min',     'wmin'),
+        ('A_plus',    'aLTP'),
+        ('A_minus',   'aLTD'),
+    )
+
+
+class GutigWeightDependence(synapses.GutigWeightDependence):
+    __doc__ = synapses.GutigWeightDependence.__doc__
+
+    translations = build_translations(
+        ('w_max',     'wmax'),
+        ('w_min',     'wmin'),
+        ('A_plus',    'aLTP'),
+        ('A_minus',   'aLTD'),
+        ('mu_plus',   'muLTP'),
+        ('mu_minus',  'muLTD'),
+    )
+
+
+class SpikePairRule(synapses.SpikePairRule):
+    __doc__ = synapses.SpikePairRule.__doc__
+
+    translations = build_translations(
+        ('tau_plus',  'tauLTP'),
+        ('tau_minus', 'tauLTD'),
+        ('A_plus',    'aLTP'),
+        ('A_minus',   'aLTD'),
+    )
