092585384cc483a9087bcc3706ac8ededf5805b1
Andrew Davison <andrew.davison@cnrs.fr>
2023-09-29 16:26:27 +0200

    Release 0.12.1

--- pyNN/__init__.py
+++ pyNN/__init__.py
@@ -69,7 +69,7 @@ Other modules:
 :license: CeCILL, see LICENSE for details.
 """
 
-__version__ = '0.12.0'
+__version__ = '0.12.1'
 __all__ = ["common", "random", "nest", "neuron", "brian2",
            "recording", "errors", "space", "descriptions",
            "standardmodels", "parameters", "core", "morphology",
