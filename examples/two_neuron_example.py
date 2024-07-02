a4c69d1e08c550bcb1886d3e7a1d5a72718681d6
Harsh Khilawala <harsh.khilawala27@gmail.com>
2024-07-02 16:16:01 -0400

    update two-neuron-example

--- examples/two_neuron_example.py
+++ examples/two_neuron_example.py
@@ -1,12 +1,28 @@
 """
 Two Neuron Example translations from NEST into PyNN.
+
+Usage: two_neuron_example.py [-h] [--plot-figure] [--debug DEBUG] simulator
+
+positional arguments:
+  simulator      neuron, nest, brian or another backend simulator
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --plot-figure  plot the simulation results to a file
+  --debug DEBUG  print debugging information
 """
 
 import pyNN.nest as sim
+from pyNN.utility import get_simulator, init_logging, normalized_filename
 from pyNN.utility.plotting import Figure, Panel 
-import matplotlib.pyplot as plt
 
-sim.setup(timestep=0.1) # (ms)
+sim, options = get_simulator(("--plot-figure", "Plot the simulation results to a file.", {"action": "store_true"}),
+                             ("--debug", "Print debugging information."))
+
+if options.debug:
+    init_logging(None, debug=True)
+
+# === Define parameters ========================================================
 
 cell_params = {
     "v_rest": -70.0, # (mV)
@@ -20,8 +36,11 @@ cell_params = {
     "i_offset": 0, # (nA)
 }
 
-cell_type = sim.IF_curr_alpha(**cell_params)
+# === Build the network ========================================================
 
+sim.setup(timestep=0.1) # (ms)
+
+cell_type = sim.IF_curr_alpha(**cell_params)
 neuron1 = sim.Population(1, cell_type, label="Neuron 1")
 neuron1.set(i_offset=0.376)
 neuron1.record("v")
@@ -30,24 +49,33 @@ neuron2 = sim.Population(1, cell_type, label="Neuron 2")
 neuron2.record("v")
 
 syn = sim.StaticSynapse(weight=0.02, delay=1.0)
-
 projection1 = sim.Projection(neuron1, neuron2, sim.AllToAllConnector(), syn)
+
+# === Run simulation ===========================================================
 
 sim.run(1000)
 
 neuron1_data_v = neuron1.get_data().segments[0].filter(name="v")[0]
 neuron2_data_v = neuron2.get_data().segments[0].filter(name="v")[0]
 
-Figure(
-    Panel(neuron1_data_v[:,0],
-          xticks=True,
-          yticks=True,
-          xlabel="Time (in ms)",
-          ylabel="Membrane Potential (in mV)"),
-    Panel(neuron2_data_v[:,0],
-          xticks=True,
-          yticks=True,
-          xlabel="Time (in ms)",
-          ylabel="Membrane Potential (in mV)")
-).show()
+if options.plot_figure:
+    figure_filename = normalized_filename("Results", "two_neuron_example", "png",
+                                          options.simulator, sim.num_processes())
 
+    Figure(
+        Panel(neuron1_data_v[:,0],
+            xticks=True,
+            yticks=True,
+            xlabel="Time (in ms)",
+            ylabel="Membrane Potential (in mV)"),
+        Panel(neuron2_data_v[:,0],
+            xticks=True,
+            yticks=True,
+            xlabel="Time (in ms)",
+            ylabel="Membrane Potential (in mV)")
+    ).save(figure_filename)
+    print(figure_filename)
+
+# === Clean up and quit ========================================================
+
+sim.end()
